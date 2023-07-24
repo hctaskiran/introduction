59cd8c03ddefcbbc5e59e5544949b42465342dea
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2023-07-24 12:15:34 +0300

    update

--- lib/intros/intropage2.dart
+++ lib/intros/intropage2.dart
@@ -1,18 +1,51 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage2 extends StatelessWidget {
   const IntroPage2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: customPage2Color().purple,
-      child: Center(child: Text('PAGE 2')),
+    return Scaffold(
+      backgroundColor: customPage2Colors().purpleColor,
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 200),
+
+            _page2Text(),
+
+            const SizedBox(height: 20),
+
+            Lottie.network('https://lottie.host/2304c971-0737-46f8-ae45-e7d7cd41c554/dkUWw78Tje.json')
+          ],
+        ),
+      ),
     );
   }
 }
 
-class customPage2Color {
-  final purple = Colors.purple.shade400;
+class customPage2Colors {
+  final purpleColor = Colors.purple.shade400;
+  final whiteColor = Colors.white;
 }
 
+
+class _page2Text extends StatelessWidget {
+  const _page2Text({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedTextKit(
+      totalRepeatCount: 1,
+      pause: Duration(seconds: 30),
+      animatedTexts: [TyperAnimatedText(
+        'Functions are simple as a pie :)',
+        textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,color: customPage2Colors().whiteColor)
+      )],
+    );
+  }
+}
--- lib/intros/intropage3.dart
+++ lib/intros/intropage3.dart
@@ -1,17 +1,55 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage3 extends StatelessWidget {
   const IntroPage3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: customPage3Color().orange,
-      child: Center(child: Text('PAGE 3')),
+    return Scaffold(
+      backgroundColor: customPage3Colors().orangeColor,
+      body: Center(
+        child: Column(
+          children: [
+
+            const SizedBox(height: 200),
+
+            _page3Text(),
+
+            const SizedBox(height: 20),
+
+            Lottie.network('https://lottie.host/1c16e794-f742-4fda-a060-fa5f435f0a5b/BiVMq4B3qY.json')
+
+          ],
+        ),
+      ),
     );
   }
 }
 
-class customPage3Color {
-  final orange = Colors.orange.shade600;
+class customPage3Colors {
+  final orangeColor = Colors.orange.shade600;
+  final whiteColor = Colors.white;
+}
+
+class _page3Text extends StatelessWidget {
+  const _page3Text({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: AnimatedTextKit(
+        totalRepeatCount: 1,
+        pause: Duration(seconds: 30),
+        animatedTexts: [TyperAnimatedText(
+          'Choose one of the activity you like and start!',
+          textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,color: customPage3Colors().whiteColor)
+        )],
+      ),
+    );
+  }
 }
